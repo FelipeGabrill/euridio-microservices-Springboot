8ffb1f254f96dd9ca39256cc1a9f6f9895a889cc
FelipeGabrill <felipegabrielodss@gmail.com>
2024-10-24 19:59:30 -0300

    Injeção de dependência com @Service e @Autowired

--- api-gatway/src/main/java/com/erudio/exceptions/handler/CustomizerResponseEntityExceptionHandler.java
+++ api-gatway/src/main/java/com/erudio/exceptions/handler/CustomizerResponseEntityExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.erudio.exceptions.ExceptionResponse;
-import com.erudio.exceptions.UnsupportedMathOperationException;
 
 @ControllerAdvice
 @RestController
@@ -24,10 +23,4 @@ public class CustomizerResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationException.class)
-	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-		
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
-	}
 }
